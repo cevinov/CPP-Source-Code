bd535907f186945fd5505855fc2465689df5eded
cevinov <94840989+cevinov@users.noreply.github.com>
2022-04-14 18:39:16 +0700

    Add files via upload

--- Program seleksi nilai beasiswa.cpp
+++ Program seleksi nilai beasiswa.cpp
@@ -2,62 +2,56 @@
 using namespace std;
 
 int main(){
-	
 cout << " Program Seleksi Beasiswa " << endl;
-	
+
 int mtk,fsk;
-
 /*
-Minimal Nilai :
-
 Matematika min 80
 Fisika min diatas 85
-
 */
 cout << endl;
-
 
 cout << "Masukkan Nilai Matematika (0-100) : ";
 cin	>> mtk;
 
 cout << "Masukkan Nilai Fisika (0-100) : ";
 cin	>> fsk;
+
+cout << endl;
+
 	
 // IF Matematika
-
 if (mtk >= 80 ){
 cout << "Nilai Matematika LULUS" << endl;
 }
-
 else{
-
  	cout << "Nilai Matematika TIDAK LULUS" << endl;
 }
+cout << endl;
+
 
 // IF Fisika
 if (fsk > 85 ) {
-
 cout << "Nilai Fisika LULUS" << endl;
 }
-
 else{
  	cout << "Nilai Fisika TIDAK LULUS" << endl;
- }
-
+}
+cout << endl;
 
 
 int total_nilai = mtk + fsk;
-
 // IF Total Nilai
-if (total_nilai >= 165 ) {
+if (total_nilai > 200 ) {
+cout << "Total Nilai TIDAK SESUAI" << endl; 	
+}
 
-cout << "Selamat Anda LULUS Seleksi Beasiswa" << endl; 
- 		
+else if (total_nilai >= 165 ){
+cout << "Selamat Anda LULUS Seleksi Beasiswa" << endl;  		
 }
 
 else if (total_nilai <= 165 ){
  	cout << "Maaf Anda TIDAK LULUS Seleksi Beasiswa" << endl;
- }
- 
+}
 }
 
