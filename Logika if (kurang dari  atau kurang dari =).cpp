af6cfde2163352fe4b6fd0f5149462efbc6fa650
cevinov <94840989+cevinov@users.noreply.github.com>
2022-04-19 13:16:24 +0700

    Add files via upload

---
+++ Logika if (kurang dari  atau kurang dari =).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+int x = 9;
+int y = 10;
+
+//if (x < y){
+if (x <= y){
+cout << "true" << endl;		
+}	
+else {
+cout << "false" << endl;	
+	
+}	
+	
+	
+}
